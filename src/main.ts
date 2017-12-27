184e07116297567ef08d2c0ae8ced1130b6b977d
Stepan <styopdev@hotmail.com>
2017-12-27 22:54:50 +0400

    Added datepickers, buttons functionality

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,52 @@
-import * as angular from "angular";
+import * as angular from 'angular';
+import * as ngMaterial from 'angular-material';
+import * as ngAnimate from 'angular-animate';
+import * as ngAria from 'angular-aria';
+import * as moment from 'moment';
+
 
 class DatepickerComponentController implements ng.IComponentController {
 
   public today: Date;
+  public dateSelectors: any[];
+  public startDate: Date;
+  public endDate: Date;
 
-  constructor() {}
+  constructor() {
+  }
 
   public $onInit () {
     this.today = new Date();
+    this.dateSelectors = [
+        {
+          name: 'Вчера',
+          dateFrom: moment().subtract(1, 'days'),
+          dateTo: moment().subtract(1, 'days')
+        }, {
+          name: 'Сегодня',
+          dateFrom: moment(),
+          dateTo: moment()
+        }, {
+          name: '2 недели',
+          dateFrom: moment().subtract(2, 'weeks'),
+          dateTo: moment()
+        },
+        {
+         name: 'Месяц',
+         dateFrom: moment().subtract(1, 'months'),
+         dateTo: moment()
+       },
+       {
+        name: 'Все',
+        dateFrom: null,
+        dateTo: null
+      },
+    ];
+  }
+
+  updateDate(data) {
+    this.startDate = data.dateFrom ? new Date(data.dateFrom) : null;
+    this.endDate = data.dateTo ? new Date(data.dateTo) : null;
   }
 }
 
@@ -21,13 +60,16 @@ class DatepickerComponent implements ng.IComponentOptions {
     this.controller = DatepickerComponentController;
     this.controllerAs = "$ctrl";
     this.template = `
-      Datepicker goes here
+      <md-datepicker ng-model='$ctrl.startDate' md-max-date='$ctrl.endDate'></md-datepicker>
+      <md-datepicker ng-model='$ctrl.endDate' md-min-date='$ctrl.startDate'></md-datepicker>
+      <br>
+      <md-button ng-repeat="ds in $ctrl.dateSelectors" ng-click="$ctrl.updateDate(ds)" ng-bind="ds.name" class="md-primary" aria-label={{ds.name}}></md-button>
     `;
   }
 }
 
 angular
-  .module("ngApp", [])
+  .module("ngApp", [ngAnimate, ngAria, ngMaterial])
   .component("datepicker", new DatepickerComponent());
 
 angular.element(document).ready(function() {
